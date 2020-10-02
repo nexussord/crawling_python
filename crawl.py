f580a9ca6a48c4605e7088f46978fa0f0befccb8
Tony Kim <nexussord@gmail.com>
2020-10-02 20:38:28 +0900

    json file creation from crawled data

--- crawl.py
+++ crawl.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import os
+
+## python file location
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ## HTTP GET Request
 req = requests.get('https://www.murung.site')
@@ -8,14 +13,30 @@ req = requests.get('https://www.murung.site')
 html = req.text
 
 ## crawl HTTP header
-header = req.headers
+## header = req.headers
 
 ## bring HTTP status (200 : normal)
-status = req.status_code
+## status = req.status_code
 
 ## check if HTTP works fine (True/False)
-is_ok = req.ok
+## is_ok = req.ok
 
 ## change html source into python elements
 ## BeautifulSoup(html source, choose which parser to use)
 soup = BeautifulSoup(html, 'html.parser')
+
+my_titles = soup.select(
+    'a'
+)
+
+data = {}
+
+for title in my_titles:
+    data[title.text] = title.get('href')
+
+print(data)
+
+with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
+    json.dump(data, json_file)
+
+
--- hotdeal_crawl.py
+++ hotdeal_crawl.py
@@ -1,22 +1,32 @@
+import json
+import os
 import requests
 from bs4 import BeautifulSoup
+
+# python file location
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 req = requests.get('https://www.fmkorea.com/hotdeal')
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
 
-## my_hotdeals is list type
+# my_hotdeals is list type
 my_hotdeals = soup.select('h3 > a')
 my_hotdeals_info = soup.select('div.hotdeal_info')
 
-
+data = {}
 
 # for hotdeals in my_hotdeals:
 #     print(hotdeals.text)
-#
+
 # for price in my_hotdeals_info:
 #     print(price.text)
 
 for hotdeals, info in zip(my_hotdeals, my_hotdeals_info):
-    print(hotdeals.text, info.text)
+    data[hotdeals.text] = info.text
+    # print(hotdeals.text, info.text)
+
+with open(os.path.join(BASE_DIR, 'hotdeal.json'), 'w+', encoding='UTF-8-sig') as json_file:
+    # korean language saved as unicode; using ensure_ascii=False fixes it
+    json.dump(data, json_file, ensure_ascii=False)
 
